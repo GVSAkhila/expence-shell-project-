9afd7384206fcd21cb4f4e1010f06b24c0cea265
Shankar <sankargv17999@gmail.com>
2024-08-28 23:18:50 +0530

    expense project

--- mysql.sh
+++ mysql.sh
@@ -42,3 +42,13 @@ mysql_secure_installation --set-root-pass ExpenseApp@1
 
 VALIDATE $? "setting the root password" 
 
+mysql -h mysql.joinsankardevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
+
