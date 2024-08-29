90dd89004446aeeef54632dd2f2066910add81b6
Shankar <sankargv17999@gmail.com>
2024-08-29 10:11:41 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
@@ -13,7 +15,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        echo -e "$R Please run this script with root privileges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
@@ -21,7 +23,7 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        echo -e "$2 is... $R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
         echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
@@ -32,25 +34,36 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install mysql-server -y &>>$LOG_FILE
-VALIDATE $? "Installing MySQL Server"
-
-systemctl enable mysqld &>>$LOG_FILE
-VALIDATE $? "Enabled MySQL Server"
-
-systemctl start mysqld &>>$LOG_FILE
-VALIDATE $? "Started MySQL server"
-
-mysql -h mysql.joinsankardevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
-    mysql_secure_installation --set-root-pass ExpenseApp@1
-    VALIDATE $? "Setting UP root password"
+# Check if MySQL is already installed
+if rpm -q mysql-server &>/dev/null; then
+    echo -e "MySQL Server is already installed... $Y SKIPPING $N" | tee -a $LOG_FILE
 else
-    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+    dnf install mysql-server -y &>>$LOG_FILE
+    VALIDATE $? "Installing MySQL Server"
 fi
 
-# Assignment
-# check MySQL Server is installed or not, enabled or not, started or not
-# implement the above things
+# Check if MySQL service is enabled
+if systemctl is-enabled mysqld &>/dev/null; then
+    echo -e "MySQL Server is already enabled... $Y SKIPPING $N" | tee -a $LOG_FILE
+else
+    systemctl enable mysqld &>>$LOG_FILE
+    VALIDATE $? "Enabling MySQL Server"
+fi
+
+# Check if MySQL service is running
+if systemctl is-active mysqld &>/dev/null; then
+    echo -e "MySQL Server is already running... $Y SKIPPING $N" | tee -a $LOG_FILE
+else
+    systemctl start mysqld &>>$LOG_FILE
+    VALIDATE $? "Starting MySQL Server"
+fi
+
+# Check if MySQL root password is set
+mysql -h mysql.joinsankardevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    echo "MySQL root password is not set up, setting now" | tee -a $LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting up root password"
+else
+    echo -e "MySQL root password is already set... $Y SKIPPING $N" | tee -a $LOG_FILE
+fi
